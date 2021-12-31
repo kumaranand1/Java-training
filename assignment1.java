647ac7ac4b5aafeb7800920248697fbd27f260be
kumaranand1 <96856525+kumaranand1@users.noreply.github.com>
2021-12-31 13:10:22 +0530

    Add files via upload
    Assignment 1 and 2 updated.

---
+++ assignment1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class maths {
+	public static void main(String[] args)
+	{
+		int num1 = 30, num2 = 10, sum = 0, sub=0, mult=0,div=0;
+
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+		
+		sum = num1 + num2;
+		System.out.println("The sum = " + sum);
+
+       sub = num1 - num2;
+		System.out.println("The sub = " + sub);
+
+		mult = num1 * num2;
+		System.out.println("The mult = " + mult);
+
+        div = num1 / num2;
+        System.out.println("The mult = " + div);
+	}
+
+}
+
---
+++ assignment2.java
@@ -0,0 +1,18 @@
+//create a java program that will find the largest of three numbers
+import java.io.*;
+
+class largestno {
+	public static void main(String[] args)
+    {
+        
+int a = 5,b = 7,c = 6,largest;
+System.out.println(" the first number:" + a); 
+System.out.println(" the second number:" + b);  
+  
+System.out.println(" the third number:" + c);  
+  
+largest = c > (a > b ? a : b) ? c : ((a > b) ? a : b);  
+System.out.println("The largest number is: "+largest); 
+    }
+
+}
